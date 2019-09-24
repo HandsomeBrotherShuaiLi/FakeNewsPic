4bd69db0fc26e609093b89bec2f8997730e89e07
TomHacker <3472661791@qq.com>
2019-09-25 02:27:59 +0800

    check the input image formats

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os,numpy as np
 from keras.applications import VGG16,VGG19,ResNet50
 from keras.applications import InceptionResNetV2,Xception,DenseNet121,DenseNet169,DenseNet201
-from keras.applications import NASNetLarge
+from keras.applications import NASNetLarge,NASNetMobile
 from keras import Model
 from keras.layers import Dense,Dropout
 import keras.backend as K
@@ -11,6 +11,7 @@ from keras.callbacks import ReduceLROnPlateau,ModelCheckpoint,EarlyStopping,Tens
 K.set_image_data_format('channels_last')
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+import pandas as pd
 # np.random.seed(2019)
 class Data(object):
     def __init__(self,shape,
@@ -18,6 +19,7 @@ class Data(object):
                  rumor_pic_dir='F:\\biendata\\task2\\train\\rumor_pic',
                  split_rate=0.1,batch_size=16):
         """
+        need to re-train
         :param shape: (height,width)
         :param true_pic_dir:
         :param rumor_pic_dir:
@@ -29,13 +31,11 @@ class Data(object):
         self.f_dir=rumor_pic_dir
         self.train_file=[]
         for i in os.listdir(self.t_dir):
-            if i.endswith('.jpg'):
-                temp = [os.path.join(self.t_dir, i), 1]
-                self.train_file.append(temp)
+            temp = [os.path.join(self.t_dir, i), 1]
+            self.train_file.append(temp)
         for i in os.listdir(self.f_dir):
-            if i.endswith('.jpg'):
-                temp = [os.path.join(self.f_dir, i), 0]
-                self.train_file.append(temp)
+            temp = [os.path.join(self.f_dir, i), 0]
+            self.train_file.append(temp)
         all_index=np.array(range(len(self.train_file)))
         val_num=int(split_rate*len(self.train_file))
         self.val_index=np.random.choice(all_index,size=val_num,replace=False)
@@ -121,7 +121,129 @@ class Mymodel(object):
                 model = Model(base_model.input, predictions)
                 model.summary()
                 return model
+        elif model_name.lower()=='resnet50':
+            base_model=ResNet50(include_top=False,input_shape=self.shape,pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable=False
+            model.summary()
+            return model
+        elif model_name.lower()=='inceptionresnetv2':
+            base_model=InceptionResNetV2(include_top=False,input_shape=self.shape,pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='xception':
+            base_model=Xception(include_top=False,input_shape=self.shape,pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='densenet121':
+            base_model = DenseNet121(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='densenet169':
+            base_model = DenseNet169(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='densenet201':
+            base_model = DenseNet201(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='nasnetlarge':
+            base_model = NASNetLarge(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        elif model_name.lower()=='vgg19':
+            base_model = VGG19(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+        else:
+            base_model = NASNetMobile(include_top=False, input_shape=self.shape, pooling='avg')
+            x = base_model.output
+            x = Dense(1024, activation='relu')(x)
+            x = Dropout(0.3)(x)
+            x = Dense(1024, activation='relu')(x)
+            predictions = Dense(1, activation='sigmoid')(x)
+            model = Model(base_model.input, predictions)
+            if fine_tune:
+                for layer in base_model.layers:
+                    layer.trainable = False
+            model.summary()
+            return model
+
     def train(self,model_name,fine_tune,optimizer='adam'):
+        if model_name.lower()=='nasnetlarge':
+            self.shape=(331,331,3)
+        elif model_name.lower()=='nasnetmobile':
+            self.shape=(224,224,3)
         data=Data(shape=self.shape,batch_size=self.batch_size,true_pic_dir=self.t_dir,
                   rumor_pic_dir=self.f_dir,split_rate=self.split_rate)
         opt=Adam(0.001) if optimizer.lower()=='adam' else SGD(0.001)
@@ -144,6 +266,55 @@ class Mymodel(object):
                                 verbose=1,save_weights_only=False,save_best_only=True)
             ]
         )
-m=Mymodel(shape=(512,512,3),batch_size=16)
-m.train(model_name='vgg16',fine_tune=False,optimizer='adam')
+    def predict(self,model_path,test_dir,fine_tune,model_name='vgg16'):
+        """
+        对于虚假新闻图片检测任务，id是图片id，label是该图片对应的预测结果，int类型，取值范围为{0,1}，
+        虚假新闻图片为1，真实新闻图片为0；
+        :param model_path:
+        :param test_dir:
+        :param fine_tune:
+        :param model_name:
+        :return:
+        """
+        if model_name.lower()=='nasnetlarge':
+            self.shape=(331,331,3)
+        elif model_name.lower()=='nasnetmobile':
+            self.shape=(224,224,3)
+        model=self.build_network(model_name,fine_tune)
+        model.load_weights(model_path)
+        fail=open('fail_predictions.txt','w',encoding='utf-8')
+        submit=pd.DataFrame(columns={'id','label'})
+        for i in os.listdir(test_dir):
+            try:
+                path = os.path.join(test_dir, i)
+                img = Image.open(path)
+                img=img.convert('RGB')
+                img = img.resize(size=(self.shape[1], self.shape[0]), resample=Image.ANTIALIAS)
+                img = np.array(img)
+                img = (img / 255.0) * 2.0 - 1.0
+                inputs=np.expand_dims(img,axis=0)
+                pred=model.predict(inputs)
+                """
+                如果pred<0.5那么应该是虚假图片，按照我的标注应该是0，但是提交要求是虚假新闻是1
+                如果pred>=0.5,那么应该是真实图片，真实图片的label是0
+                """
+                print(i,pred[0][0])
+                if pred[0][0]<=0.5:
+                    submit=submit.append({'id':i.split('.')[0],'label':1},ignore_index=True)
+                else:
+                    submit=submit.append({'id':i.split('.')[0],'label':0},ignore_index=True)
+            except:
+                fail.write(str(i.split('.')[0])+'\n')
+        submit.to_csv('submit_v1.csv',index=False)
+        fail.close()
 
+m=Mymodel(shape=(256,256,3),batch_size=16)
+# m.build_network(model_name='resnet50',fine_tune=True)
+# m.train(model_name='resnet50',fine_tune=False,optimizer='adam')
+m.predict(
+    model_path='models/resnet50_adam_-008--0.31349--0.85968.hdf5',
+    model_name='resnet50',fine_tune=False,test_dir='/media/lishuai/Newsmy/biendata/task2/stage1_test'
+)
+# m.predict(model_path='models/vgg16_fine_tune_adam_-002--0.67326--0.59946.hdf5',
+#           model_name='vgg16',fine_tune=True,test_dir='F:\\biendata\\task2\\stage1_test')
+#tail -fn 50 nohup.out
---
+++ preload.py
@@ -0,0 +1,60 @@
+from PIL import Image
+import os,numpy as np
+from collections import defaultdict
+"""
+defaultdict(<class 'int'>, {'jpg': 19882, 'gif': 471, 'jpeg': 13, 'png': 84, '96acb7a83da068b22297e4f972d1bcb1': 1, 'a765ede36caccb10ac311aa745a9b96a': 1, '28d7eef2921631a91ce76d57182dde64': 1, 'e90088dc9a4b15fbab834ad49338f288': 1, '8e8909d42bd36c629291b3943eebae7a': 1, 'bmp': 1, '29ad0ab7967c3426f806e7cfaeedc803': 1, 'd20c9e59bc8d161896cf510cd048fe7b': 1, '8908e2a96b96bab0dd4f7b4546130383': 1, 'aspx': 1, '32b634458ef43e715744b81b7f92a782': 1})
+defaultdict(<class 'int'>, {'jpg': 13523, 'gif': 78, 'c2b22e8b8e9926b640725447ea763207': 1, 'e7a4012e58b922f2425d9917c5e951d0': 1, '68ffa949c420a69b42d688ed4a017a9d': 1, '4bfdd70c758f603c0ef3a978d1c0c012': 1, 'b7120291167b1727c1578d1f9b918fdf': 1, 'aspx': 1, 'ba5a82bf2413b37c56cc9121a8736b7b': 1, '1bbbc0c03c68f044e7b319c5f1973d8e': 1, 'png': 4, '5815bd9efaa7c908f2c15ccd81a87942': 1, '521023883607a8763e153b4e1f02f51d': 1, 'c7c679d8a13f626caf018ed49e3026ee': 1, '336ea3a9f5b003c2f26f933997eada94': 1, 'ebf055dbe26f79bfa8167b88dab31baa': 1, 'dc87d5de47bb54243978c6c4ba38ed74': 1, '933b9f5b8a8be817c44a50fdd5645b3b': 1, 'jpeg': 1, '94fcb35b7a3badbc41ed7ef1f4fb8e60': 1, 'df99d07f2ab3af3ffc094c1cb24bdcfd': 1, 'a88190a3ac4c75c28aaea34cb19336cf': 1, 'a882e6514356d006b8334e6ad7ba8e14': 1, '96acb7a83da068b22297e4f972d1bcb1': 1, 'b43a3b5bc8b89da6a34b083c6ecefb96': 1, '241daec4a7f0b1d6cbed7cd4dd4c07e4': 1, '21be089edb3e181538e5e6d0ed2d32bb': 1, 'c6f55453818abf8f23e7b864ea7b74a1': 1, '1b51b9f4521db9f897bf609e62ee6c47': 1, '2aff75591944b59643de6e56b100b0a8': 1, 'e59e28d8b97b9f0d140d14c0940f012e': 1, '45851620f23d29d5612c87e362b2da47': 1, '9f67a3af3d559d0a1c5731ab505c0ee4': 1})
+defaultdict(<class 'int'>, {'jpg': 3773, 'png': 10, 'gif': 50, 'jpeg': 2, '63f1d89eccbd25f4aa2731b3426d99a9': 1, 'f6dcf73eb8b4fd61af2b4e257f288568': 1})
+"""
+def checktype():
+    truth_dir='/media/lishuai/Newsmy/biendata/task2/train/truth_pic'
+    rumor_dir='/media/lishuai/Newsmy/biendata/task2/train/rumor_pic'
+    test_dir='/media/lishuai/Newsmy/biendata/task2/stage1_test'
+    truth_types=defaultdict(int)
+    gif,sb,jpeg,png=None,None,None,None
+    for i in os.listdir(truth_dir):
+        t=i.split('.')[-1]
+        if t=='gif':
+            gif=os.path.join(truth_dir,i)
+        if len(i.split('.'))==1:
+            sb=os.path.join(truth_dir,i)
+        if t=='jpeg':
+            jpeg=os.path.join(truth_dir,i)
+        if t=='png':
+            png=os.path.join(truth_dir,i)
+        truth_types[t]+=1
+    rumor_types=defaultdict(int)
+    for i in os.listdir(rumor_dir):
+        rumor_types[i.split('.')[-1]]+=1
+    test_types=defaultdict(int)
+    for i in os.listdir(test_dir):
+        test_types[i.split('.')[-1]]+=1
+    print(truth_types)
+    print(rumor_types)
+    print(test_types)
+    try:
+        img=Image.open(gif).convert('RGB')
+        img=np.array(img)
+        print('gif shape',img.shape)
+    except Exception as e:
+        print('gif error:{}'.format(e))
+    try:
+        img = Image.open(jpeg)
+        img = np.array(img)
+        print('jpeg shape',img.shape)
+    except Exception as e:
+        print('jpeg error:{}'.format(e))
+    try:
+        img = Image.open(sb)
+        img = np.array(img)
+        print('sb shape',img.shape)
+    except Exception as e:
+        print('sb error:{}'.format(e))
+    try:
+        img = Image.open(png)
+        img = np.array(img)
+        print('png shape',img.shape)
+    except Exception as e:
+        print('png error:{}'.format(e))
+checktype()
+
